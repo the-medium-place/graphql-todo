c6fbad84e6016005ce1bed99d71bf16c213339d7
ZGS <zgstowell@gmail.com>
2021-06-29 12:49:46 -0700

    trying to deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/graphql-todo',
+  process.env.MONGODB_URI || 'mongodb://localhost/gql-todo',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
